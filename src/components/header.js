c922b2d34f2e8bcca6eead9a20c3af0d67dd60df
yoonsun lee <yoonsunlee150@gmail.com>
2020-03-26 13:37:01 -0400

    Simple UI updates
    Ui updates

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,18 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import Link from "gatsby-link"
+import './Header.css'
+
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src={require('../images/apple1.png')} width="45" /><img src={require('../images/nicebl-01.svg')} width="70" /></Link>
+      <Link to="/index">Home</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/buy"><button>Buy</button></Link>
     </div>
-  </header>
+
+  </div>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,19 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
+
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+
+
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Collecting Rainbows</h1>
+        <p>Send me all your Rainbows</p>
+        <Link to="#"><button>View All</button></Link>
+
+      </div>
+
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
